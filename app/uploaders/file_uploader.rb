17a4e901db655a97570355beaa97b777dd8f63e3
Josh Rendek <josh@bluescripts.net>
2011-10-07 19:51:21 -0400

    task uploading fix for heroku

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -17,7 +17,11 @@ class FileUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.task_id}"
   end
-
+  
+  def cache_dir
+    "#{RAILS_ROOT}/tmp/uploads"
+  end
+  
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
