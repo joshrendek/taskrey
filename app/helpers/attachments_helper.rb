b7de592c9f14ac3822ea4d28f9c3d0bb6b37a708
Josh Rendek <josh@bluescripts.net>
2011-10-07 20:57:43 -0400

    better styles on attachments

--- app/helpers/attachments_helper.rb
+++ app/helpers/attachments_helper.rb
@@ -2,11 +2,14 @@ module AttachmentsHelper
   def attachment_helper(a)
     file_name = attachment_name(a)
     file_type = MIME::Types.type_for(file_name).join('/').split('/')[0]
+    output = ''
     if file_type == "image"
-      link_to image_tag(a.attachment, {:size => "128x128"}) , a.attachment.to_s, :target => "_blank"
+      output << link_to(image_tag(a.attachment, {:size => "128x128"}) , a.attachment.to_s, :target => "_blank")
+      output << attachment_name(a)
     else
-      link_to file_name, a.attachment.to_s, :target => "_blank" 
+      output << link_to(file_name, a.attachment.to_s, :target => "_blank")
     end
+    output
   end
 
   def attachment_name(a)
